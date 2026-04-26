170d739824c0a63866cfb7270075660c7cb68b8c
Arijit Kumar Roy <83859073+arijitkroy@users.noreply.github.com>
2026-04-26 21:01:02 +0530

    sorting

---
+++ sorting/udp_client.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define C_IP "127.0.0.1"
+#define C_PORT 15555
+#define S_IP "127.0.0.1"
+#define S_PORT 15550
+
+int main() {
+	struct sockaddr_in client, server;
+	int s, clen = sizeof(client), slen = sizeof(server), n, i, arr[512];
+	char msg[512];
+	bzero((char *)&client, clen);
+	client.sin_family = AF_INET;
+	client.sin_addr.s_addr = inet_addr(C_IP);
+	client.sin_port = htons(C_PORT);
+	bzero((char *)&server, slen);
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr(S_IP);
+	server.sin_port = htons(S_PORT);
+	s = socket(AF_INET, SOCK_DGRAM, 0);
+	while (strcmp(msg, "stop")) {
+		printf("Enter size of array: ");
+		scanf("%d", &n);
+		printf("Enter elements: ");
+		for (i = 0; i < n; i++) scanf("%d", &arr[i]);
+		sendto(s, arr, n * sizeof(int), 0, (struct sockaddr *)&server, slen);
+		recvfrom(s, arr, 512, 0, (struct sockaddr *)&server, &slen);
+		printf("Sorted elements: ");
+		for (i = 0; i < n; i++) printf("%d ", arr[i]);
+		printf("\n");
+	}
+	close(s);
+	return 0;
+}
---
+++ sorting/udp_server.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include <time.h>
+
+#define S_IP "127.0.0.1"
+#define S_PORT 15550
+
+void bubble(int arr[], int n) {
+	int i, j, temp;
+	for (i = 0; i < n - 1; i++) {
+		for (j = 0; j < n - i - 1; j++) {
+			if (arr[j+1] < arr[j]) {
+				temp = arr[j+1];
+				arr[j+1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+}
+
+int main() {
+	struct sockaddr_in client, server;
+	int s, clen = sizeof(client), slen = sizeof(server), arr[512], n;
+	char msg[512], *t;
+	time_t t1;
+	bzero((char *)&server, slen);
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr(S_IP);
+	server.sin_port = htons(S_PORT);
+	s = socket(AF_INET, SOCK_DGRAM, 0);
+	bind(s, (struct sockaddr *)&server, slen);
+	while (strcmp(msg, "stop")) {
+		n = recvfrom(s, arr, 512, 0, (struct sockaddr *)&client, &clen);
+		bubble(arr, n / sizeof(int));
+		sendto(s, arr, n, 0, (struct sockaddr *)&client, clen);
+		/*
+		memset(msg, 0x0, 512);
+		recvfrom(s, msg, 512, 0, (struct sockaddr *)&client, &clen);
+		if (strcmp(msg, "time") == 0) {
+			t1 = time(NULL);
+			t = ctime(&t1);
+			sendto(s, t, strlen(t)+1, 0, (struct sockaddr *)&client, clen);
+		}
+		else sendto(s, msg, strlen(msg)+1, 0, (struct sockaddr *)&client, clen);
+		*/
+	}
+	close(s);
+	return 0;
+}
